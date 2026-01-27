30d948267ff29c457d50b86eb94ec4936dbea7e0
bisma <gyndarab@gmail.com>
2026-01-27 18:18:38 +0700

    marker

--- app.py
+++ app.py
@@ -6,4 +6,32 @@ import numpy as np
 from streamlit_folium import st_folium
 import matplotlib.pyplot as plt
 
-st.write('Hello, Saya Bisma')
+df = pd.read_excel('scraping_kosan.xlsx')
+
+# Buat peta
+center_lat = -6.8907
+center_lon = 107.542
+m = folium.Map(location=[center_lat, center_lon], zoom_start=10)
+
+
+st.title('ANALISIS DAN REKOMENDASI PENETAPAN HARGA KOS DI BERBAGAI AREA')
+st.write('Hasil analisis kelompok kami, akan memberikan informasi kepada ' \
+'wirausaha yang ingin membangun kos-kosan, berapa harga yang tepat serta berapa fasilitas yang perlu diberikan di setiap areanya.')
+
+# Tambahkan marker
+for idx, row in df.iterrows():
+    if str(row['Kota']).strip().lower() == 'bandung':
+        folium.Marker(
+            location=[row['Latitude'], row['Longitude']],
+            popup=(
+                f"Kota: {row['Kota']}<br>"
+                f"Latitude: {row['Latitude']}<br>"
+                f"Longitude: {row['Longitude']}"
+            ),
+            tooltip="Klik untuk info"
+        ).add_to(m)
+
+
+# Tampilkan peta di Streamlit
+st.title("Peta Kosan")
+st_folium(m, width=700, height=500)
