2f9dbb4eff076ad6c97d8a2f3e5c71c84e2bd4cf
bisma <gyndarab@gmail.com>
2026-01-29 15:54:19 +0700

    add another area preview

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ st.write('Hasil analisis kelompok kami, akan memberikan informasi kepada ' \
 jumlah_kosan_per_kota = df["Kota"].value_counts()
 
 gabungan_kota = {
-    "Bandung": ["Bandung", "Kabupaten Bandung"],
+    "Bandung": ["Bandung", "Kabupaten Bandung", 'Kabupaten Bandung Barat'],
     "Yogyakarta": ["Yogyakarta", "Kabupaten Bantul"],
     "Solo (Surakarta)": ["Solo (Surakarta)", "Kabupaten Karanganyar"]
 }
@@ -31,6 +31,7 @@ for kota, group in polygon.groupby("Kota"):
 
     coords = group[["Latitude", "Longitude"]].values.tolist()
 
+    #menghitung gabungan kota
     if kota in gabungan_kota:
         jumlah_kosan = sum(
             jumlah_kosan_per_kota.get(k, 0)
@@ -159,3 +160,96 @@ for kota, group in polygon.groupby("Kota"):
 
 st.title('Bogor')
 st_folium(b, width=700, height=500)
+
+center_lat_yogya = -7.738462636007786 
+center_lon_yogya = 110.3976623661559
+
+y = folium.Map(location=[center_lat_yogya, center_lon_yogya], zoom_start=12)
+
+for kota, group in polygon.groupby("Kota"):
+
+    coords = group[["Latitude", "Longitude"]].values.tolist()
+
+    if kota in gabungan_kota:
+        jumlah_kosan = sum(
+            jumlah_kosan_per_kota.get(k, 0)
+            for k in gabungan_kota[kota]
+        )
+    else:
+        jumlah_kosan = jumlah_kosan_per_kota.get(kota, 0)
+
+    if (jumlah_kosan <= 50):
+        polygon_color = "yellow"
+    elif (jumlah_kosan <= 80):
+        polygon_color = "orange"
+    elif (jumlah_kosan <= 150):
+        polygon_color = "red"
+    else:
+        polygon_color = "blue"
+
+    if len(coords) >= 3:
+        folium.Polygon(
+            locations=coords,
+            color=polygon_color,
+            fill=True,
+            fill_color=polygon_color,
+            fill_opacity=0.4
+        ).add_to(y)
+
+    folium.Marker(
+        location=[group["Latitude"].mean(), group["Longitude"].mean()],
+        tooltip=kota,
+        popup=f"{kota}<br>Jumlah kosan: {jumlah_kosan}",
+        icon=folium.Icon(color="blue", icon="info-sign")
+    ).add_to(y)
+
+
+st.title('Yogyakarta dan Kabupaten Sleman')
+st_folium(y, width=700, height=500)
+
+
+center_lat_semarang = -7.001714208585145 
+center_lon_semarang = 110.3976623661559
+
+s = folium.Map(location=[center_lat_semarang, center_lon_semarang], zoom_start=12)
+
+for kota, group in polygon.groupby("Kota"):
+
+    coords = group[["Latitude", "Longitude"]].values.tolist()
+
+    if kota in gabungan_kota:
+        jumlah_kosan = sum(
+            jumlah_kosan_per_kota.get(k, 0)
+            for k in gabungan_kota[kota]
+        )
+    else:
+        jumlah_kosan = jumlah_kosan_per_kota.get(kota, 0)
+
+    if (jumlah_kosan <= 50):
+        polygon_color = "yellow"
+    elif (jumlah_kosan <= 80):
+        polygon_color = "orange"
+    elif (jumlah_kosan <= 150):
+        polygon_color = "red"
+    else:
+        polygon_color = "blue"
+
+    if len(coords) >= 3:
+        folium.Polygon(
+            locations=coords,
+            color=polygon_color,
+            fill=True,
+            fill_color=polygon_color,
+            fill_opacity=0.4
+        ).add_to(s)
+
+    folium.Marker(
+        location=[group["Latitude"].mean(), group["Longitude"].mean()],
+        tooltip=kota,
+        popup=f"{kota}<br>Jumlah kosan: {jumlah_kosan}",
+        icon=folium.Icon(color="blue", icon="info-sign")
+    ).add_to(s)
+
+
+st.title('Semarang')
+st_folium(s, width=700, height=500)
