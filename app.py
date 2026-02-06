1ba5ba4002587dafe44637ef716a72395f2b22c0
bisma <gyndarab@gmail.com>
2026-02-06 21:40:26 +0700

    FINAL

--- app.py
+++ app.py
@@ -18,6 +18,10 @@ rumah = pd.read_excel('data/HargaRumah.xlsx')
 avg_harga_per_kota = df.groupby("Kota")["Harga (Rp)"].mean().to_dict()
 jumlah_kosan_per_kota = df["Kota"].value_counts()
 
+kelas1 = 84
+kelas2 = 156
+kelas3 = 180
+
 # Gabungan wilayah
 gabungan_kota = {
     "Bandung": ["Bandung", "Kabupaten Bandung", "Kabupaten Bandung Barat"],
@@ -69,12 +73,12 @@ if selected_city in gabungan_fasilitas:
     df_filtered = df[df["Kota"].isin(gabungan_fasilitas[selected_city])]
     grafik_filtered = grafik[grafik["Kota"].isin(gabungan_fasilitas[selected_city])]
     grafikMakan_filtered = grafikMakan[grafikMakan["Kota"].isin(gabungan_fasilitas[selected_city])]
-    kota_aktif = gabungan_fasilitas[selected_city]
+    df_HargaRumah = rumah[rumah['Kota'].isin(gabungan_fasilitas[selected_city])]
 else:
     df_filtered = df[df["Kota"] == selected_city]
     grafik_filtered = grafik[grafik["Kota"] == selected_city]
     grafikMakan_filtered = grafikMakan[grafikMakan["Kota"] == selected_city]
-    kota_aktif = [selected_city]
+    df_HargaRumah = rumah[rumah['Kota'] == selected_city]
 
 rumah_filtered = rumah.copy()
 
@@ -282,11 +286,8 @@ with col4:
 st.divider()
 
 st.subheader('Lama pelunasan Rumah')
-
-df_filtered = df[df["Kota"].isin(kota_aktif)]
-grafik_filtered = grafik[grafik["Kota"].isin(kota_aktif)]
-grafikMakan_filtered = grafikMakan[grafikMakan["Kota"].isin(kota_aktif)]
-
+st.write('Perhitungan ini didapatkan dengan cara menghitung sisa gaji setelah membayar biaya sewa rumah dan juga biaya makan perbulan, lalu membagi harga rumah dengan sisa gaji')
+# Gabungkan UMR + harga kos + biaya makan
 df_keuangan = (
     grafik_filtered[["Kota", "UMK", "ratarata"]]
     .merge(
@@ -296,28 +297,84 @@ df_keuangan = (
     )
 )
 
-df_keuangan["Sisa Gaji / Bulan"] = (
-    df_keuangan["UMK"] - (df_keuangan["ratarata"] + df_keuangan["Biaya Makan"])
+# Hitung sisa gaji per bulan
+df_keuangan["Sisa UMR / Bulan"] = (
+    df_keuangan["UMK"]
+    - (df_keuangan["ratarata"] + df_keuangan["Biaya Makan"])
 )
 
-df_rumah_hasil = df_keuangan.merge(
-    rumah,
-    how="cross"
+# Gabungkan dengan harga rumah
+df_lama_pelunasan = (
+    df_keuangan[["Kota", "Sisa UMR / Bulan"]]
+    .merge(
+        df_HargaRumah[["Kota", "Harga"]],
+        on="Kota",
+        how="inner"
+    )
 )
 
-df_rumah_hasil["Lama Pelunasan (Tahun)"] = (
-    df_rumah_hasil["Harga"] / df_rumah_hasil["Sisa Gaji / Bulan"] / 12
+# Hitung lama pelunasan (tahun)
+df_lama_pelunasan["Lama Pelunasan (Tahun)"] = (
+    df_lama_pelunasan["Harga"]
+    / df_lama_pelunasan["Sisa UMR / Bulan"]
+    / 12
 ).round(1)
 
-df_rumah_hasil = df_rumah_hasil[[
+df_lama_pelunasan = df_lama_pelunasan[[
     "Kota",
-    "Tipe",
+    "Sisa UMR / Bulan",
     "Harga",
     "Lama Pelunasan (Tahun)"
 ]]
 
 st.dataframe(
-    df_rumah_hasil,
+    df_lama_pelunasan,
+    use_container_width=True,
+    hide_index=True
+)
+
+st.subheader('UMR ideal ditiap daerah')
+st.write('perhitungan ideal ini berdasarkan biaya yang dikeluarkan untuk sewa tempat tinggal, biaya makan perbulan')
+
+rumah_kelas1 = rumah[rumah["Kelas"] == "Kelas 1"]
+
+df_ideal = (
+    grafik_filtered[["Kota", "UMK", "ratarata"]]
+    .merge(
+        grafikMakan_filtered[["Kota", "Biaya Makan"]],
+        on="Kota",
+        how="inner"
+    )
+    .merge(
+        rumah_kelas1[["Kota", "Harga"]],
+        on="Kota",
+        how="inner"
+    )
+)
+
+target_bulan = 7 * 12
+
+df_ideal["Sisa Ideal / Bulan"] = df_ideal["Harga"] / target_bulan
+
+df_ideal["UMR Baru"] = (
+    df_ideal["Sisa Ideal / Bulan"]
+    + df_ideal["ratarata"]
+    + df_ideal["Biaya Makan"]
+).round(0)
+
+df_ideal["Nilai Kenaikan (Rp)"] = (
+    df_ideal["UMR Baru"] - df_ideal["UMK"]
+)
+
+df_umr_ideal = df_ideal[[
+    "Kota",
+    "UMK",
+    "Nilai Kenaikan (Rp)",
+    "UMR Baru"
+]]
+
+st.dataframe(
+    df_umr_ideal,
     use_container_width=True,
     hide_index=True
 )
